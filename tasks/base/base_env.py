b3132ae3fb07d76b819d91c3250f074d2e8759fb
Augusto Mota Pinheiro <augustomp55@gmail.com>
2024-06-09 10:31:43 -0400

    chore: removed unecessary formatting settings

--- tasks/base/base_env.py
+++ tasks/base/base_env.py
@@ -1,6 +1,7 @@
 import gc
 from abc import abstractmethod
 
+import isaacsim
 from omni.isaac.kit import SimulationApp
 
 config = {"headless": False}
@@ -10,6 +11,13 @@ from omni.isaac.core import World
 from omni.isaac.core.scenes.scene import Scene
 from omni.isaac.core.utils.stage import create_new_stage_async, update_stage_async
 
+# TODO: separate into 3 classes: BaseEnv, BaseTask, BaseAgent
+# BaseEnv: contains the world, tasks, agents, and settings (mother class, if you will)
+# BaseTask: contains the task settings and the task logic
+# BaseAgent: contains the agent settings and the agent logic
+# The goal is to make it so that any task can be run in any environment with any agent
+# Need to figure out how abc works in python and how to use it to enforce the structure of the classes
+
 
 class BaseEnv(object):
     def __init__(self) -> None:
@@ -18,7 +26,7 @@ class BaseEnv(object):
         self._world_settings = {
             "physics_dt": 1.0 / 60.0,
             "stage_units_in_neters": 1.0,
-            "rendereing_dt": 1.0 / 60.0,
+            "rendering_dt": 1.0 / 60.0,
         }
         return
 
@@ -26,5 +34,5 @@ class BaseEnv(object):
 world = World()
 world.scene.add_default_ground_plane()
 
-while True:
+while sim_app.is_running():
     world.step(render=True)
