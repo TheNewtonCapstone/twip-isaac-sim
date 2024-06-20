087cfab876251caa8272193065aa280ae96a3ced
Augusto Mota Pinheiro <augustomp55@gmail.com>
2024-06-19 23:00:51 -0400

    feat: added config file & basic reading

--- core/twip/twip.py
+++ core/twip/twip.py
@@ -1,6 +1,8 @@
 import os
 import hydra
 import torch
+from rl_games.common import env_configurations, vecenv
+
 
 from isaacsim import SimulationApp
 from rl_games.torch_runner import Runner
@@ -8,6 +10,25 @@ from rl_games.torch_runner import Runner
 from core.envs.generic_env import GenericEnv
 from core.twip.twip_agent import TwipAgent, WheelDriveType
 from core.twip.generic_task import GenericTask
+
+from omegaconf import DictConfig, OmegaConf
+from typing import Dict
+
+
+@hydra.main(config_name="config", config_path="./../../configs/")
+def load_config(cfg: DictConfig) -> DictConfig:
+    return cfg
+
+
+def omegaconf_to_dict(d: DictConfig) -> Dict:
+    ret = {}
+    exit(1)
+    for k, v in d.items():
+        if isinstance(v, DictConfig):
+            ret[k] = omegaconf_to_dict(v)
+        else:
+            ret[k] = v
+    return ret
 
 
 def get_current_path() -> str:
@@ -42,12 +63,16 @@ if __name__ == "__main__":
 
     twip.set_target_velocity(WheelDriveType.LEFT, 0)
     twip.set_target_velocity(WheelDriveType.RIGHT, 1200)
+    # config_dict = omegaconf_to_dict(load_config())
 
-    runner = Runner()
+    # vecenv.register("RLGPU", lambda config_name, num_actors, **kwargs: GenericTask(env))
+    # runner = Runner()
+    # runner.load(config_dict)
+    # runner.reset()
 
-    runner.run({"train": True, "play": False})
+    # runner.run({"train": True, "play": False})
 
-    while False and sim_app.is_running():
+    while sim_app.is_running():
         task.step(torch.zeros(1))
 
         if env.o_world.current_time % 10 <= 5:
