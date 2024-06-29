c53fd0dae47634a929a055ef862c7e8466707aba
Augusto Mota Pinheiro <augustomp55@gmail.com>
2024-06-29 10:53:50 -0400

    feat: first, somewhat usable, training configuration & task!

--- core/twip/generic_task.py
+++ core/twip/generic_task.py
@@ -10,35 +10,44 @@ from rl_games.common.ivecenv import IVecEnv
 from core.base.base_task import BaseTask
 from core.base.base_env import BaseEnv
 
+from core.twip.twip_agent import WheelDriveType
+
 
 class GenericTask(BaseTask):
     def __init__(self, _base_env: BaseEnv):
         super().__init__(_base_env)
 
-    def load_config(self):
+    def load_config(self, headless=True):
         config = {}
         config["device"] = "cuda:0"
-        config["headless"] = False
+        config["headless"] = headless
 
         config["num_envs"] = 1
 
         config["num_agents"] = 1
-        config["num_observations"] = 10
+        config["num_observations"] = 4
         config["num_actions"] = 2
         config["num_states"] = 0
 
         config["observation_space"] = gm.spaces.Box(
-            np.ones(config["num_observations"]) * -np.Inf,
-            np.ones(config["num_observations"]) * np.Inf,
+            low=np.array(
+                [
+                    -np.pi,
+                    -np.Inf,
+                    -600.0,
+                    -600.0,
+                ]
+            ),
+            high=np.array([np.pi, np.Inf, 600.0, 600.0]),
         )
         config["action_space"] = gm.spaces.Box(
-            np.ones(config["num_actions"]) * -1.0,
-            np.ones(config["num_actions"]) * 1.0,
-        )  # not sure why it's different from the other spaces
+            np.ones(config["num_actions"]) * -600.0,
+            np.ones(config["num_actions"]) * 600.0,
+        )
         config["state_space"] = gm.spaces.Box(
             np.ones(config["num_states"]) * -np.Inf,
             np.ones(config["num_states"]) * np.Inf,
-        )  # still not sure what a state is
+        )
 
         # task-specific config
         config["domain_randomization"] = {}
@@ -48,9 +57,9 @@ class GenericTask(BaseTask):
         return super().load_config(config)
 
     def construct(self, sim_app: SimulationApp) -> bool:
-        c_res = self.base_env.construct(sim_app)
+        res = self.base_env.construct(sim_app)
 
-        if not c_res:
+        if not res:
             return False
 
         self.base_env.prepare()
@@ -70,33 +79,48 @@ class GenericTask(BaseTask):
 
         twip_agent = self.base_env.agent
 
+        twip_agent.set_target_velocity(WheelDriveType.LEFT, actions[0, 0].item())
+        twip_agent.set_target_velocity(WheelDriveType.RIGHT, actions[0, 1].item())
+
         self.base_env.step(_render=not self.headless)
 
         twip_obs_dict = twip_agent.get_observations()
-        twip_obs = torch.cat(
-            (
-                twip_obs_dict["lin_acc"],
-                twip_obs_dict["ang_vel"],
-                twip_obs_dict["orientation"],
-            ),
-            0,
+        twip_pitch = self._quaternion_to_euler(twip_obs_dict["orientation"])[0]
+
+        twip_obs = torch.tensor(
+            [
+                twip_pitch,
+                twip_obs_dict["ang_vel"][0],
+                actions[0, 0].item(),
+                actions[0, 1].item(),
+            ],
         )
         obs = {
-            "obs": torch.cat((twip_obs.unsqueeze(0), torch.tensor([])), 0).to(
-                device=self.device
+            "obs": torch.tensor(
+                twip_obs.unsqueeze(0), device=self.device, dtype=torch.float32
             )
         }
 
-        # reward is the difference between current orientation and stable orientation
-        reward = torch.tensor(
-            np.linalg.norm(twip_obs_dict["orientation"] - np.array([1, 0, 0, 0]))
+        combined_applied_vel = np.abs(actions[0, 0].item()) + np.abs(
+            actions[0, 1].item()
         )
 
-        done = False
+        # the smaller the difference between current orientation and stable orientation, the higher the reward
+        reward = torch.tensor(
+            [
+                1.0
+                - np.tanh(8 * np.abs(twip_pitch))
+                - 0.05 * np.tanh(np.abs(twip_obs_dict["ang_vel"][2]))
+                - 0.05 * np.tanh(combined_applied_vel)
+            ]
+        )
 
-        # reset if the orientation is too far off
-        if reward > 0.1:
-            done = True
+        if np.abs(twip_pitch) > 0.2:
+            done = torch.tensor([True])
+            reward = torch.tensor([-4.0])
+            self.reset()
+        else:
+            done = torch.tensor([False])
 
         env_info = self.get_env_info()
 
@@ -112,21 +136,36 @@ class GenericTask(BaseTask):
 
         self.base_env.prepare()
 
+        # makes sure that the twip is stable before starting
+        for _ in range(4):
+            self.base_env.step(_render=not self.headless)
+
         twip_obs_dict = twip_agent.get_observations()
-        twip_obs = torch.cat(
-            (
-                twip_obs_dict["lin_acc"],
-                twip_obs_dict["ang_vel"],
-                twip_obs_dict["orientation"],
-            ),
-            0,
+        twip_obs = torch.tensor(
+            [
+                self._quaternion_to_euler(twip_obs_dict["orientation"])[0],
+                twip_obs_dict["ang_vel"][0],
+                0.0,
+                0.0,
+            ],
         )
         obs = {
-            "obs": torch.cat((twip_obs.unsqueeze(0), torch.tensor([])), 0).to(
-                device=self.device
+            "obs": torch.tensor(
+                twip_obs.unsqueeze(0), device=self.device, dtype=torch.float32
             )
         }
 
-        print(obs)
-
         return obs
+
+    def _quaternion_to_euler(self, q: np.ndarray) -> np.ndarray:
+        # converts a quaternion to euler angles
+        # args: quaternion
+        # returns: euler angles
+
+        w, x, y, z = q[0], q[1], q[2], q[3]
+
+        roll = np.arctan2(2 * (w * x + y * z), 1 - 2 * (x**2 + y**2))
+        pitch = np.arcsin(2 * (w * y - z * x))
+        yaw = np.arctan2(2 * (w * z + x * y), 1 - 2 * (y**2 + z**2))
+
+        return np.array([roll, pitch, yaw])
--- core/twip/twip.py
+++ core/twip/twip.py
@@ -1,11 +1,11 @@
 import os
 import hydra
 import torch
-from rl_games.common import env_configurations, vecenv
-
 
 from isaacsim import SimulationApp
 from rl_games.torch_runner import Runner
+from rl_games.common.algo_observer import IsaacAlgoObserver
+from rl_games.common import env_configurations, vecenv
 
 from core.envs.generic_env import GenericEnv
 from core.twip.twip_agent import TwipAgent, WheelDriveType
@@ -27,7 +27,7 @@ app_settings = {
 }
 
 world_settings = {
-    "physics_dt": 1.0 / 60.0,
+    "physics_dt": 1.0 / 200.0,
     "stage_units_in_meters": 1.0,
     "rendering_dt": 1.0 / 60.0,
 }
@@ -39,6 +39,8 @@ twip_settings = {
 
 @hydra.main(config_name="config", config_path="./configs")
 def train(cfg: DictConfig) -> Dict:
+    is_rl = True
+
     sim_app = SimulationApp(app_settings)
 
     env = GenericEnv(world_settings)
@@ -46,14 +48,10 @@ def train(cfg: DictConfig) -> Dict:
 
     env.add_agent(twip)
 
-    task = GenericTask(env)
-    task.load_config()
-    task.construct(sim_app)
-
     task_architect = base_task_architect(env, sim_app, GenericTask)
 
     twip.set_target_velocity(WheelDriveType.LEFT, 0)
-    twip.set_target_velocity(WheelDriveType.RIGHT, 1200)
+    twip.set_target_velocity(WheelDriveType.RIGHT, 0)
 
     task_config = OmegaConf.to_container(cfg, resolve=True)
 
@@ -66,19 +64,27 @@ def train(cfg: DictConfig) -> Dict:
     )
     vecenv.register("RLGPU", lambda config_name, num_actors, **kwargs: task_architect())
 
-    # runner = Runner()
-    # runner.load(task_config)
-    # runner.reset()
+    if is_rl:
+        runner = Runner(IsaacAlgoObserver())
+        runner.load(task_config)
+        runner.reset()
 
-    # runner.run({"train": True, "play": False})
+        runner.run({"train": True, "play": False})
 
-    while sim_app.is_running():
-        task.step(torch.zeros(1))
+    if not is_rl:
+        env.construct(sim_app)
 
+        env.prepare()
+
+    while not is_rl and sim_app.is_running():
         if env.o_world.current_time % 10 <= 5:
             twip.set_target_velocity(WheelDriveType.LEFT, 0)
         else:
             twip.set_target_velocity(WheelDriveType.LEFT, 1400)
+
+        env.step(not app_settings["headless"])
+
+        print(twip.get_observations())
 
 
 if __name__ == "__main__":
