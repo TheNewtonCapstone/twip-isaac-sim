5662bdb809bbb5eaebe0fd2d665f2976d078cd1d
Augusto Mota Pinheiro <augustomp55@gmail.com>
2024-07-23 13:40:07 -0400

    Vectorized TWIP training (#2)
    * wip: beginning of new training environment
    
    * chore: removed unecessary formatting settings
    
    * feat: import URDF works
    
    * feat: successfully imported working robot
    
    * feat: separated base functionality into their respective classes
    
    * chore: renamed "tasks" -> "core"
    
    * feat: added IMU sensor to twip
    
    * feat: created somewhat of a base task class
    
    * chore: switched to Python API for ground plane spawning
    
    * feat: completed implementation of IVecEnv
    
    * feat: hooked up env & agent to task
    
    * wip: added base rl_games runner
    
    * feat: added config file & basic reading
    
    * chore: ignore new links to isaac sim
    
    * fix: eliminated errors from loading the config
    
    * feat: cleaned up observations from twip_agent
    
    * feat: further progressed on observations for a self-balancing twip
    
    * fix: simulation works again
    
    * feat: first, somewhat usable, training configuration & task!
    
    * feat: updated balancing config
    
    * chore: cleaned up project
    
    * wip: vectorizing envs
    
    * feat: added multiple envs
    
    * feat: made ArticulationView work
    
    * Fixed _isaac_sim directory issue
    
    * chore: cleaned & adjusted GPU memory allocation values
    
    * feat: added cli args & completed refactor
    
    * feat: Implemented the RobotView as well as the Replicator
    
    * Revert "feat: Implemented the RobotView as well as the Replicator"
    
    * feat: implemented vectorized imu reading
    
    * feat: Introduced the replicator and changed the backend
    
    * feat: Refactored the world setup
    
    * chore: added an "experience" file to config isaac sim & its extensions
    
    * feat: completed vectorized training setup
    
    * chore: cleaned up some things
    
    * feat: added alternative IMU sensor
    
    * feat: adjusted reward calculation
    
    * feat: added episodic resets
    
    * feat: added export feature to twip
    
    * docs(imu): modified docs to specify the reference frame
    
    * Delete checkpoints/.hydra/hydra.yaml
    
    * Delete checkpoints/.hydra/config.yaml
    
    * Delete checkpoints/.hydra/overrides.yaml
    
    * chore: removed checkpoints folder
    
    ---------
    
    Co-authored-by: Houssam Righi <hrighi01@gmail.com>

---
+++ core/base/base_env.py
@@ -0,0 +1,70 @@
+from abc import ABC, abstractmethod
+from typing import Dict
+import torch
+
+from core.base.base_agent import BaseAgent
+
+# TODO: separate into 3 classes: BaseEnv, BaseTask, BaseAgent
+# BaseEnv: contains the world, agents and settings
+# BaseTask: contains the world (with its agents) task settings and the task logic
+# BaseAgent: contains the agent settings and the agent logic
+# The goal is to make it so that any task can be run in any environment with any agent
+# Need to figure out how abc works in python and how to use it to enforce the structure of the classes
+
+
+class BaseEnv(ABC):
+    def __init__(
+        self,
+        world_settings: Dict,
+        num_envs: int,
+    ) -> None:
+        self.world_settings = world_settings
+        self.num_envs = num_envs
+
+    @abstractmethod
+    def construct(self, agent: BaseAgent) -> str:
+        self.agent = agent  # save the agent class for informative purposes (i.e. introspection/debugging)
+
+        import omni.isaac.core
+        from omni.isaac.core import World
+        from omni.isaac.core.utils.stage import get_current_stage
+        from pxr import Sdf, UsdLux
+
+        stage = get_current_stage()
+
+        self.world: World = World(
+            physics_dt=self.world_settings["physics_dt"],
+            rendering_dt=self.world_settings["rendering_dt"],
+            stage_units_in_meters=self.world_settings["stage_units_in_meters"],
+            backend=self.world_settings["backend"],
+            device=self.world_settings["device"],
+        )
+
+        # Adjust physics scene settings (mainly for GPU memory allocation)
+        phys_context = self.world.get_physics_context()
+        phys_context.set_gpu_found_lost_aggregate_pairs_capacity(
+            max(self.num_envs * 64, 1024)
+        )  # 1024 is the default value, eyeballed the other value
+        phys_context.set_gpu_total_aggregate_pairs_capacity(
+            max(self.num_envs * 64, 1024)
+        )  # 1024 is the default value, eyeballed the other value
+
+        # Add sun
+        sun = UsdLux.DistantLight.Define(stage, Sdf.Path("/distantLight"))
+        sun.CreateIntensityAttr(500)
+
+        return None
+
+    @abstractmethod
+    def step(self, actions: torch.Tensor, render: bool) -> torch.Tensor:
+        self.world.step(render=render)
+        return None
+
+    @abstractmethod
+    def reset(
+        self,
+        indices: torch.Tensor = None,
+    ) -> Dict[str, torch.Tensor]:
+        self.world.reset()
+
+        return None
