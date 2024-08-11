eaf5267d631aa58f8716e00de1852e66c99fb70a
Augusto Mota Pinheiro <augustomp55@gmail.com>
2024-08-11 15:46:54 -0400

    wip : interminent commit, new reward works with dr

--- core/domain_randomizer/domain_randomizer.py
+++ core/domain_randomizer/domain_randomizer.py
@@ -1,11 +1,13 @@
 import torch
 import numpy as np
 
+
 class DomainRandomizer:
     def __init__(self, world, twip_art_view, randomization_params):
 
         import omni.replicator.isaac as dr
         import omni.replicator.core as rep
+
         self.dr = dr
         self.rep = rep
 
@@ -24,7 +26,6 @@ class DomainRandomizer:
 
         self.format_randomization_params()
 
-
         self.frame_idx = 0
 
         # Register the simulation context and articulation view
@@ -36,18 +37,22 @@ class DomainRandomizer:
         def process_property(distribution, range_values):
             range_str = self.get_randomization_range(range_values)
             if distribution == "uniform":
-                return self.rep.distribution.uniform(tuple(range_str[0]), tuple(range_str[1]))
+                return self.rep.distribution.uniform(
+                    tuple(range_str[0]), tuple(range_str[1])
+                )
             elif distribution == "normal":
-                return self.rep.distribution.normal(tuple(range_str[0]), tuple(range_str[1]))
+                return self.rep.distribution.normal(
+                    tuple(range_str[0]), tuple(range_str[1])
+                )
             else:
                 raise ValueError(f"Invalid distribution type: {distribution}")
 
         def format_properties(properties):
             return {
                 prop: process_property(
-                    prop_data.get('distribution', 'uniform'),
-                    prop_data.get('range', [])
-                ) for prop, prop_data in properties.items()
+                    prop_data.get("distribution", "uniform"), prop_data.get("range", [])
+                )
+                for prop, prop_data in properties.items()
             }
 
         formatted_params = {}
@@ -57,13 +62,17 @@ class DomainRandomizer:
             gate_type_config = self.domain_params.get(gate_type, {})
             formatted_params[gate_type] = {}
 
-            for property_type in ["articulation_view_properties", "dof_properties", "rigid_body_properties"]:
+            for property_type in [
+                "articulation_view_properties",
+                "dof_properties",
+                "rigid_body_properties",
+            ]:
                 property_config = gate_type_config.get(property_type, {})
 
                 formatted_params[gate_type][property_type] = {
-                    'additive': format_properties(property_config.get('additive', {})),
-                    'scaling': format_properties(property_config.get('scaling', {})),
-                    'direct': format_properties(property_config.get('direct', {}))
+                    "additive": format_properties(property_config.get("additive", {})),
+                    "scaling": format_properties(property_config.get("scaling", {})),
+                    "direct": format_properties(property_config.get("direct", {})),
                 }
 
         self.on_interval_properties = formatted_params.get("on_interval", {})
@@ -96,10 +105,21 @@ class DomainRandomizer:
                                 **args,
                             )
 
+            with self.dr.gate.on_env_reset():
+                for body in self.on_reset_properties:
+                    if "articulation_view_properties" in body:
+                        for prop in self.on_reset_properties[body]:
+                            body_properties = self.on_reset_properties.get(body, {})
+                            args = body_properties.get(prop, {})
+                            self.dr.physics_view.randomize_articulation_view(
+                                view_name=self.twip_art_view.name,
+                                operation=str(prop),
+                                **args,
+                            )
 
     def step_randomization(self):
         reset_inds = []
         if self.frame_idx % 200 == 0:
             reset_inds = np.arange(self.twip_art_view.count)
-        self.dr.physics_view.step_randomization()
+        self.dr.physics_view.step_randomization(reset_inds=reset_inds)
         self.frame_idx += 1
