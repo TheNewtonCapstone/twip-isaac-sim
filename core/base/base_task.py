ddeb0a6755688fc66fd665ff7e8711e1673729d4
Augusto Mota Pinheiro <augustomp55@gmail.com>
2024-10-04 10:36:52 -0400

    feat: changed `base_task.py` to use STB3

--- core/base/base_task.py
+++ core/base/base_task.py
@@ -1,143 +1,110 @@
+from typing import Dict, Any, Callable, List, Optional, Sequence, Type
+
 import gymnasium as gym
+from stable_baselines3.common.vec_env import VecEnv
+from stable_baselines3.common.vec_env.base_vec_env import VecEnvIndices, VecEnvStepReturn, VecEnvObs
+
 import numpy as np
-import torch
-from typing import Dict, Any, Tuple, Callable, List
-
-from rl_games.common.ivecenv import IVecEnv
-
-from core.base.base_env import BaseEnv
 from core.base.base_agent import BaseAgent
+from core.base.base_env import BaseEnv
 
 
-class BaseTask(IVecEnv):
-    def __init__(
-        self,
-        training_env_factory: Callable[..., BaseEnv],
-        playing_env_factory: Callable[..., BaseEnv],
-        agent_factory: Callable[..., BaseAgent],
-    ):
+class BaseTask(VecEnv):
+    def __init__(self, headless: bool, device: str, num_envs: int, playing: bool, observation_space: gym.spaces.Box,
+                 action_space: gym.spaces.Box, reward_space: gym.spaces.Box, config: Dict[str, Any],
+                 training_env_factory: Callable[..., BaseEnv], playing_env_factory: Callable[..., BaseEnv],
+                 agent_factory: Callable[..., BaseAgent], ):
+        super(BaseTask, self).__init__(num_envs=num_envs, observation_space=observation_space,
+                                       action_space=action_space)
+
         self.config = {}
 
         self.training_env_factory = training_env_factory
         self.playing_env_factory = playing_env_factory
         self.agent_factory = agent_factory
 
-        self.agent: BaseAgent = None
+        self.agent: BaseAgent | None = None
         self.envs: List[BaseEnv] = []
 
-    def load_config(
-        self,
-        headless: bool,
-        device: str,
-        num_envs: int,
-        playing: bool,
-        config: Dict[str, Any],
-    ) -> None:
         self.config: Dict[str, Any] = config
 
         self.headless: bool = headless
         self.device: str = device
         self.playing: bool = playing
 
+        self.observation_space: gym.spaces.Box = observation_space
+        self.action_space: gym.spaces.Box = action_space
+        self.reward_space: gym.spaces.Box = reward_space
+
         self.num_envs: int = num_envs
         self.max_episode_length: int = self.config["max_episode_length"]
 
-        self.num_agents: int = self.config["num_agents"]
         self.num_observations: int = self.config["num_observations"]
         self.num_actions: int = self.config["num_actions"]
         self.num_states: int = self.config["num_states"]
-
-        self.observation_space: gym.spaces.Box = self.config["observation_space"]
-        self.action_space: gym.spaces.Box = self.config["action_space"]
-        self.state_space: gym.spaces.Box = self.config["state_space"]
+        self.actions: np.ndarray = np.zeros((self.num_envs, self.num_actions), dtype=np.float32)
 
         self.domain_randomization: bool = self.config.get("domain_randomization", False)
 
         self._setup_buffers()
 
-        # rest of config inside self.config
-
     def __str__(self):
-        return f"{self.__class__.__name__} with {self.num_envs} environments, {self.num_agents} agents, {self.num_observations} observations, {self.num_actions} actions, {self.num_states} states."
+        return f"{self.__class__.__name__} with {self.num_envs} environments, {self.num_observations} observations, {self.num_actions} actions, {self.num_states} states."
 
     def construct(self) -> bool:
         pass
 
     def _setup_buffers(self) -> None:
-        self.obs_buf = torch.zeros(
-            self.num_envs,
-            self.num_observations,
-            device=self.device,
-            dtype=torch.float32,
-        )
-        self.rewards_buf = torch.zeros(
-            self.num_envs,
-            device=self.device,
-            dtype=torch.float32,
-        )
-        self.dones_buf = torch.zeros(
-            self.num_envs,
-            device=self.device,
-            dtype=torch.bool,
-        )
-        self.progress_buf = torch.zeros(
-            self.num_envs,
-            device=self.device,
-            dtype=torch.long,
-        )
+        self.obs_buf = np.zeros((self.num_envs, self.num_observations,), dtype=np.float32)
+        self.rewards_buf = np.zeros((self.num_envs,), dtype=np.float32)
+        self.dones_buf = np.zeros((self.num_envs,), dtype=np.bool_)
+        self.progress_buf = np.zeros((self.num_envs,), dtype=np.ulonglong)
 
-    # RL-Games methods (required from IVecEnv)
+    # Gymnasium methods (required from VecEnv)
 
-    def step(
-        self, actions: torch.Tensor = None
-    ) -> Tuple[Dict[str, torch.Tensor], torch.Tensor, torch.Tensor, Dict[str, Any]]:
-        # goes through a RL step (includes all the base RL things such as get obs, apply actions, etc.
-        # args: actions to apply to the env
-        # returns: obs, rewards, resets, info
+    def reset(self) -> VecEnvObs:
         pass
 
-    def reset(self) -> Dict[str, torch.Tensor]:
-        # resets a single environment
-        # returns: the observations
+    def step_async(self, actions: np.ndarray) -> None:
+        self.actions = actions
+        return
+
+    def step_wait(self) -> VecEnvStepReturn:
         pass
 
-    def seed(self, seed) -> None:
+    def close(self) -> None:
         pass
 
-    def has_action_masks(self) -> bool:
-        return False
-
-    def get_number_of_agents(self) -> int:
-        # we only support 1 agent per env for now
-        return 1
-
-    def get_env_info(self) -> Dict:
-        return {
-            "observation_space": self.observation_space,
-            "action_space": self.action_space,
-            "state_space": self.state_space,
-        }
-
-    def set_train_info(self, env_frames, *args, **kwargs):
+    def seed(self, seed: Optional[int] = None) -> Sequence[None | int]:
         pass
 
-    def get_env_state(self):
-        return None
+    # Helper methods (shouldn't need to be overridden)
 
-    def set_env_state(self, env_state) -> None:
-        pass
+    def get_attr(self, attr_name: str, indices: VecEnvIndices = None) -> List[Any]:
+        """Return attribute from vectorized environment (see base class)."""
+        target_envs = self._get_target_envs(indices)
+        return [getattr(env_i, attr_name) for env_i in target_envs]
 
-    def get_observation_space(self) -> gym.spaces:
-        return self.observation_space
+    def set_attr(self, attr_name: str, value: Any, indices: VecEnvIndices = None) -> None:
+        """Set attribute inside vectorized environments (see base class)."""
+        target_envs = self._get_target_envs(indices)
+        for env_i in target_envs:
+            setattr(env_i, attr_name, value)
 
-    def get_action_space(self) -> gym.Space:
-        return self.action_space
+    def env_method(self, method_name: str, *method_args, indices: VecEnvIndices = None, **method_kwargs) -> List[Any]:
+        """Call instance methods of vectorized environments."""
+        target_envs = self._get_target_envs(indices)
+        return [getattr(env_i, method_name)(*method_args, **method_kwargs) for env_i in target_envs]
 
-    def get_state_space(self) -> gym.spaces.Box:
-        return self.state_space
+    def env_is_wrapped(self, wrapper_class: Type[gym.Wrapper], indices: VecEnvIndices = None) -> List[bool]:
+        """Check if worker environments are wrapped with a given wrapper"""
+        target_envs = self._get_target_envs(indices)
+        # Import here to avoid a circular import
+        from stable_baselines3.common import env_util
 
-    def get_num_envs(self) -> int:
-        return self.num_envs
+        return [env_util.is_wrapped(env_i, wrapper_class) for env_i in target_envs]
 
-    def get_device(self) -> str:
-        return self.device
+    def _get_target_envs(self, indices: VecEnvIndices) -> List[gym.Env]:
+        indices = self._get_indices(indices)
+        # noinspection PyTypeChecker
+        return [self.envs[i] for i in indices]
